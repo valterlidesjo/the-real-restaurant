01f1b121c6b2e7d7622aa220ff6111190d89d199
Miwilicious <viklund.mikaela@gmail.com>
2025-02-11 22:51:04 +0100

    Last changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,21 +3,23 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="max-w-6xl mx-auto bg-white p-8">
+    <footer className="w-full bg-white p-8">
       <div className="text-center mb-4">
-        <h2 className="text-2xl italic font-semibold">{contact.name}</h2>
+        <h2 className="text-2xl logo text-[3rem] font-bold md:text-[4rem]">
+          {contact.name}
+        </h2>
       </div>
-      <div className="flex justify-between items-start">
+      <div className="flex flex-col md:flex-row md:justify-between items-start">
         <div className="text-left">
           <p className="font-bold uppercase text-sm">Make Reservation &gt;</p>
-          <p>{contact.address}</p>
+          <p className="py-4">{contact.address}</p>
           <p>{contact.location}</p>
         </div>
-        <div className="text-right">
+        <div className="text-left md:text-right mt-4 md:mt-0">
           <p className="font-bold uppercase text-sm">Get in touch</p>
           <a
             href={`mailto:${contact.email}`}
-            className="text-black-600 underline"
+            className="text-black-600 underline inline-block py-4"
           >
             {contact.email}
           </a>
