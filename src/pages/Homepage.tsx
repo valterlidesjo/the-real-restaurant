95b48deb5ac3e0391716111e46bf25e37a69622a
valterlidesjo <valle.lidesjo@gmail.com>
2025-02-11 15:12:11 +0100

    styling exept font

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import pizza from "../assets/pizza-size.png";
 
 const Homepage = () => {
   return (
-    <div className='text-1xl'>Homepage</div>
+    <div className='w-full flex justify-center items-center flex-col'>
+      <img src={pizza} alt="Pizza" className='w-3/4 my-16'/>
+      <p className='text-3xl mb-2'>Fitzgerald Av. 5</p>
+      <p className='text-3xl mb-8'>Kingston, Jamaica</p>
+      <p className='text-3xl mb-2'>18.00 - 00.00</p>
+      <p className='text-3xl'>Every day</p>
+
+    </div>
   )
 }
 
