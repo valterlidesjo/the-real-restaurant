64f1de0537c7f2e00e27e31d7592d4542bacab5a
valterlidesjo <valle.lidesjo@gmail.com>
2025-02-12 13:00:00 +0100

    input started

--- src/components/UI/TimeInput.tsx
+++ src/components/UI/TimeInput.tsx
@@ -1,27 +1,44 @@
+import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 
 interface TimeInputProps {
-    header: string;
-    chosen: string;
+  header: string;
+  chosen: string;
+  
 }
-
 
 const TimeInput: React.FC<TimeInputProps> = ({ header, chosen }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <>
-    <div className="w-4/5 flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
-            <p>{header}</p>
-            <p>{chosen}</p>
+      <div className="w-4/5 border border-black border-solid">
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-col justify-center items-center ml-2 p-2">
+            <p className="text-[0.6rem]">{header}</p>
+            <p className="text-sm">{chosen}</p>
+          </div>
+          <div className="mr-2" onClick={handleClick}>
+            <IoIosArrowDown />
+          </div>
         </div>
-        <div><IoIosArrowDown/></div>
 
-        {/* <div className="h-0 flex flex-col justify-center items-center p-8">
-            <p>Hidden information</p>
-        </div> */}
-    </div>
+        <div
+          className={
+            isOpen
+              ? "h-28 flex flex-col justify-center items-center p-8"
+              : "hidden"
+          }
+        >
+          <p>Hidden information</p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TimeInput
+export default TimeInput;
--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReservationText from "../components/ReservationText";
+import TimeInput from "../components/UI/TimeInput";
 
 const Reservation = () => {
   return (
     <>
       <ReservationText />
+      <TimeInput header="Guests" chosen="2 Guests"/>
     </>
   );
 };
