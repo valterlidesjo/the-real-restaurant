48292917bed6100658238ff6886a0e43d25c3f8c
Miwilicious <viklund.mikaela@gmail.com>
2025-02-17 16:40:25 +0100

    New footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,29 +3,30 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-white p-8">
-      <div className="text-center mb-4">
+    <footer className="w-full bg-cream p-8 mt-8">
+      <div className="text-center mb-2">
         <h2 className="text-2xl logo text-[3rem] font-bold md:text-[4rem]">
           {contact.name}
         </h2>
+        <p className="pb-4 italic">{contact.location}</p>
       </div>
-      <div className="flex flex-col md:flex-row md:justify-between items-start">
-        <div className="text-left">
-          <p className="font-bold uppercase text-sm">Make Reservation &gt;</p>
-          <p className="py-4">{contact.address}</p>
-          <p>{contact.location}</p>
-        </div>
-        <div className="text-left md:text-right mt-4 md:mt-0">
-          <p className="font-bold uppercase text-sm">Get in touch</p>
-          <a
-            href={`mailto:${contact.email}`}
-            className="text-black-600 underline inline-block py-4"
-          >
-            {contact.email}
-          </a>
-          <p>{contact.phone}</p>
-        </div>
+
+      <div className="flex justify-around items-center mt-5 links w-full md:w-1/2 mx-auto">
+        <a href="/" className="font-bold uppercase text-sm">
+          Home
+        </a>
+        <span className="block sm:hidden">&#x2022;</span>
+        <a href="/reservation" className="font-bold uppercase text-sm">
+          Reservation
+        </a>
+        <span className="block sm:hidden">&#x2022;</span>
+        <a href="/contact" className="font-bold uppercase text-sm">
+          Contact
+        </a>
       </div>
+      <p className="small text-center mt-7">
+        © 2025 Terni. All rights reserved.
+      </p>
     </footer>
   );
 };
