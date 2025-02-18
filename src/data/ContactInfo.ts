afe29e7c7a374643e97f46c7c6de7587665a7e28
Miwilicious <viklund.mikaela@gmail.com>
2025-02-18 15:10:21 +0100

    contact page fix

--- src/data/ContactInfo.ts
+++ src/data/ContactInfo.ts
@@ -4,4 +4,5 @@ export const contact = {
   address: "Fitzgerald Av. 5",
   location: "Kingston, Jamaica",
   phone: "+ 1337 420 420",
+  hours: "18:00 - 00:00",
 };
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import { FiPhone, FiMail } from "react-icons/fi";
 import ContactMap from "../components/ContactMap/ContactMap";
+import { contact } from "../data/ContactInfo";
 
 const Contact = () => {
   return (
@@ -8,27 +9,27 @@ const Contact = () => {
         <div className="map test-cursor my-5">
           <ContactMap />
         </div>
-        <p className="text-2xl mb-1">Fitzgerald Av. 5</p>
-        <p className="text-2xl mb-8">Kingston, Jamaica</p>
-        <p className="text-2xl mb-1">18.00 - 00.00</p>
+        <p className="text-2xl mb-1">{contact.address}</p>
+        <p className="text-2xl mb-8">{contact.location}</p>
+        <p className="text-2xl mb-1">{contact.hours}</p>
         <p className="text-2xl mb-8">Every day</p>
 
-        <div className="flex justity-center items-center w-full">
-          <div className="w-[35%] flex justify-evenly items-end flex-col h-32 md:w-[45%]">
-            <div className="w-10 h-10 border-solid border-black border-2 flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-full space-y-4">
+          <div className="flex items-center">
+            <div className="w-10 h-10 border-2 border-black flex justify-center items-center">
               <FiPhone className="text-2xl" />
             </div>
-            <div className="w-10 h-10 border-solid border-black border-2 flex justify-center items-center">
+            <p className="text-[1.35rem] pl-4">{contact.phone}</p>
+          </div>
+          <div className="flex items-center">
+            <div className="w-10 h-10 border-2 border-black flex justify-center items-center">
               <FiMail className="text-2xl" />
             </div>
-          </div>
-          <div className="w-[65%] flex justify-evenly items-start flex-col h-32 md:w-[55%]">
-            <p className="text-[1.35rem] pl-4">+1337 420 420</p>
             <a
-              href="mailto:info@terni.kingston?subject=Contact Terni"
+              href={`mailto:${contact.email}?subject=Contact ${contact.name}`}
               className="text-[1.35rem] pl-4"
             >
-              info@terni.kingston
+              {contact.email}
             </a>
           </div>
         </div>
