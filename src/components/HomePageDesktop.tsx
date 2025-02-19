41622a0e9080f352ead5b6af447ec83dcae4466d
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-02-19 11:08:59 +0100

    video on homepage now loops, and text-slider added on mobile

--- src/components/HomePageDesktop.tsx
+++ src/components/HomePageDesktop.tsx
@@ -5,7 +5,7 @@ import pizza from "../assets/image 21.svg";
 import mozzarella from "../assets/image 14.svg";
 import TextSection from "./TextSection";
 import InfiniteSlider from "./UI/InfiniteSlider";
-import { InfiniteTextData } from "./data/InfiniteTextData";
+import { InfiniteTextDataDesktop } from "./data/InfiniteTextData";
 
 const HomePageDesktop = () => {
   return (
@@ -18,20 +18,20 @@ const HomePageDesktop = () => {
             className="object-cover w-full h-[500px] mt-[1.6125rem]"
           />
         </div>
-        <InfiniteSlider text={InfiniteTextData} />
+        <InfiniteSlider text={InfiniteTextDataDesktop} />
         <div className="flex w-full">
           <img src={pizza} alt="" className="object-cover w-[50%] h-[500px]" />
           <div className="w-[50%] box-border p-8">
             <TextSection
               header="OUR MENU"
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-              link=""
+              link="menu"
               linkText="Find out more"
             />
           </div>
         </div>
         <div className="flex w-full">
-          <div className="w-[50%] box-border p-[2rem]">
+          <div className="w-[50%] box-border p-8">
             <TextSection
               header="EAT GOOD LIVE GOOD"
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.Lorem ipsum dolor sit amet, consectetur adipiscing elit"
@@ -43,6 +43,7 @@ const HomePageDesktop = () => {
               controls
               autoPlay
               loop
+              muted
               className="object-cover h-[250px]"
             />
             <img src={mozzarella} alt="" className="object-cover h-[250px]" />
--- src/components/HomePageMobile.tsx
+++ src/components/HomePageMobile.tsx
@@ -5,45 +5,46 @@ import pizza from "../assets/image 21.svg";
 import mozzarella from "../assets/image 14.svg";
 import { Link } from "react-router";
 import TextSection from "../components/TextSection";
-import SearchComponent from "../components/SearchComponent";
-import InfiniteTextScroll from "../components/UI/InfiniteSlider";
-import { InfiniteTextData } from "../components/data/InfiniteTextData";
-import InfiniteSlider from "../components/UI/InfiniteSlider";
+import { InfiniteTextDataMobile } from "./data/InfiniteTextData";
+import InfiniteSlider from "./UI/InfiniteSlider";
 
 const HomePageMobile = () => {
   return (
-    <div className="w-full flex justify-center items-center flex-col gap-8">
-      <div className="w-full">
-        <img
-          src={Hero}
-          alt="Hero Image"
-          className="object-cover w-full h-[300px] mt-[1.6125rem]"
-        />
-      </div>
-
-      <div className="flex flex-col p-8 gap-8">
-        <TextSection
-          header="OUR MENU"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    <>
+      <section className="w-full flex flex-col gap-8">
+        <div className="w-full">
+          <img
+            src={Hero}
+            alt="Hero Image"
+            className="object-cover w-full h-[300px] mt-[1.6125rem]"
+          />
+        </div>
+        <InfiniteSlider text={InfiniteTextDataMobile} />
+        <div className="flex flex-col px-8 gap-8">
+          <TextSection
+            header="OUR MENU"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          link=""
-          linkText="Find out more"
-        />
-        <video
-          src={Video}
-          controls
-          autoPlay
-          loop
-          className="object-cover h-[300px]"
-        />
-        <TextSection
-          header="EAT GOOD LIVE GOOD"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            link=""
+            linkText="Find out more"
+          />
+          <video
+            src={Video}
+            controls
+            autoPlay
+            loop
+            muted
+            className="object-cover h-[300px]"
+          />
+          <TextSection
+            header="EAT GOOD LIVE GOOD"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <img src={pizza} alt="" className="object-cover h-[300px]" />
-      </div>
-    </div>
+          />
+          <img src={pizza} alt="" className="object-cover h-[300px]" />
+        </div>
+      </section>
+    </>
   );
 };
 
